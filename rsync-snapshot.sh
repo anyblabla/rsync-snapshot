e7cfb9a644095c967c27e142faed9102fdd589e7
Amaury Libert <amaury-libert@hotmail.com>
2022-08-02 12:09:47 +0200

    Update rsync-snapshot.sh
    Ajout de toutes les lignes "#"

--- rsync-snapshot.sh
+++ rsync-snapshot.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+#
+# Autheur:
+#   Amaury Libert <amaury-libert@hotmail.com> de Blabla Linux <https://blablalinux.be>
+#
+# Description:
+#   A script to perform full incremental backups of a server using rsync - Here for a Nextcloud Dedicated Server   
+#   Un script pour effectuer des sauvegardes incrémentielles complètes d'un serveur à l'aide de rsync - Ici pour un serveur dédié Nextcloud
+#
+# Préambule Légal:
+# 	Ce script est un logiciel libre.
+# 	Vous pouvez le redistribuer et / ou le modifier selon les termes de la licence publique générale GNU telle que publiée par la Free Software Foundation; version 3.
+#
+# 	Ce script est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE; sans même la garantie implicite de QUALITÉ MARCHANDE ou d'ADÉQUATION À UN USAGE PARTICULIER.
+# 	Voir la licence publique générale GNU pour plus de détails.
+#
+# 	Licence publique générale GNU : <https://www.gnu.org/licenses/gpl-3.0.txt>
+#
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 MAILTO=""
-
-# A script to perform full incremental backups of a server using rsync - Here for a Nextcloud Dedicated Server
-# Un script pour effectuer des sauvegardes incrémentielles complètes d un serveur à l aide de rsync - Ici pour un serveur dédié Nextcloud
 
 set -o errexit
 set -o nounset
